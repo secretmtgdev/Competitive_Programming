455d25fe81d3046f9169069596f0b8afdd34deea
Michael Wilson <amcwils@amazon.com>
2020-10-18 16:19:33 -0700

    Added follow along code from Tushar's all permutations and CodeNCode's kth prime

--- Algorithms/stringAlgorithms.cpp
+++ Algorithms/stringAlgorithms.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <map>
+#include <tuple>
+#include <vector>
 
 using namespace std;
 
@@ -7,10 +9,12 @@ using namespace std;
 // FUNCTION DECLARATION //
 //////////////////////////
 void combinationOfChars(string);
+void permutations(string);
 
 int main()
 {
     combinationOfChars("abc");
+    permutations("aabc");
 }
 
 /////////////////////////////
@@ -18,7 +22,6 @@ int main()
 /////////////////////////////
 void print(string out, int len)
 {
-    // cout << "Printing contents, length = " << len << endl;
     for (int i = 0; i < len; i++)
     {
         cout << out[i];
@@ -26,11 +29,10 @@ void print(string out, int len)
     cout << endl;
 }
 
-void combinationHelper(string str, string out, int pos, int count[], int len)
+void combinationHelper(string str, string out, int pos, vector<int> count, int len)
 {
     print(out, len);
     int n = str.length();
-    // cout << "Size = " << n << endl;
     for (int i = pos; i < n; i++)
     {
         if (count[i] != 0)
@@ -43,7 +45,7 @@ void combinationHelper(string str, string out, int pos, int count[], int len)
     }
 }
 
-void combinationOfChars(string s)
+tuple<string, vector<int>> getCharCounts(string s)
 {
     map<char, int> uniqueCharCounts;
     for (int i = 0; i < s.length(); i++)
@@ -58,32 +60,63 @@ void combinationOfChars(string s)
             uniqueCharCounts.insert(make_pair(c, 1));
         }
     }
-    // cout << "Printing map contents" << endl;
-    // for (auto pair : uniqueCharCounts)
-    // {
-    //     printf("(%c, %d)\n", pair.first, pair.second);
-    // }
+
     size_t mapSize = uniqueCharCounts.size();
 
     // indication of termination point in string
     // this is to avoid garbarge and an overstepping
     // of contents
     string str(mapSize, '\0');
-    int count[mapSize];
+    vector<int> count;
     int i = 0;
     for (auto const &[key, val] : uniqueCharCounts)
     {
-        str[i] = key;
-        count[i] = val;
-        i++;
+        str[i++] = key;
+        count.push_back(val);
     }
+    return make_tuple(str, count);
+}
 
-    // for (int i = 0; i < mapSize; i++)
-    // {
-    //     printf("Char: %c, Count: %d\n", str[i], count[i]);
-    // }
+void combinationOfChars(string s)
+{
+    tuple<string, vector<int>> charCountTuple = getCharCounts(s);
     string output(s.length(), '\0');
+
+    string str = get<0>(charCountTuple);
+    vector<int> count = get<1>(charCountTuple);
 
     printf("Printing all linear combinations of %s", s.c_str());
     combinationHelper(str, output, 0, count, 0);
+}
+
+void permutationsHelper(string str, string out, vector<int> count, int len)
+{
+    if (len == out.length())
+    {
+        print(out, out.length());
+    }
+    int n = str.length();
+    for (int i = 0; i < n; i++)
+    {
+        if (count[i] != 0)
+        {
+            out[len] = str[i];
+            count[i]--;
+            permutationsHelper(str, out, count, len + 1);
+            count[i]++;
+        }
+    }
+}
+
+void permutations(string s)
+{
+    tuple<string, vector<int>> charCountTuple = getCharCounts(s);
+    string output(s.length(), '\0');
+
+    string str = get<0>(charCountTuple);
+    vector<int> count = get<1>(charCountTuple);
+
+    printf("Printing all permutations of %s", s.c_str());
+
+    permutationsHelper(str, output, count, 0);
 }
---
+++ Number_Theory/primes.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+///////////////////////////
+// FUNCTION DECLARATIONS //
+///////////////////////////
+void timeFunction(string, function<bool(ll)>);
+bool isPrimeNaive(ll);
+bool isPrimeSqrt(ll);
+bool isPrimeSieve(ll);
+void generateSieveArray(ll *);
+void kthPrime();
+
+int main()
+{
+    // timeFunction("Naive", isPrimeNaive);
+    // timeFunction("Sqrt", isPrimeSqrt);
+    // timeFunction("Sieve", isPrimeSieve);
+    kthPrime();
+}
+
+//////////////////////////////
+// FUNCTION IMPLEMENTATIONS //
+//////////////////////////////
+bool isPrimeNaive(ll n)
+{
+    if (n < 2)
+        return false;
+    for (ll i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isPrimeSqrt(ll n)
+{
+    if (n < 2)
+        return false;
+    for (ll i = 2; i * i < n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+bool isPrimeSieve(ll n)
+{
+    ll sieveArray[n];
+    generateSieveArray(sieveArray);
+    return sieveArray[n - 1];
+}
+
+//////////////////////
+// HELPER FUNCTIONS //
+//////////////////////
+void timeFunction(string functionName, function<bool(ll)> callback)
+{
+    auto start = chrono::high_resolution_clock::now();
+    auto result = callback(LLONG_MAX);
+    auto stop = chrono::high_resolution_clock::now();
+    ll elapsedTime = chrono::duration_cast<chrono::nanoseconds>(stop - start).count();
+    printf("Function %s took a total of %lld nanoseconds\n", functionName.c_str(), elapsedTime);
+}
+
+void generateSieveArray(ll *sieve)
+{
+    ll n = sizeof(sieve) / sizeof(*sieve);
+
+    // Assume the contents of the array are all primes
+    for (ll i = 0; i < n; i++)
+    {
+        sieve[i] = 1;
+    }
+
+    // first value in the sieves, aka '1', can't be prime
+    sieve[0] = 0;
+    for (ll i = 1; i * i < n; i++)
+    {
+        if (sieve[i])
+        {
+            ll startFactor = 2;
+            for (int j = i * i; i <= n; j += i)
+            {
+                sieve[j] = 0;
+            }
+        }
+    }
+}
+
+vector<int> kthPrimeHelper()
+{
+    int k = 1000000;
+    bool notPrimes[k]; // false by default
+    vector<int> primes;
+    // zero and 1 are not primes
+    notPrimes[0] = notPrimes[1] = true;
+    for (int i = 2; i * i < k; i++)
+    {
+        // found a value that is considered to be a prime
+        if (!notPrimes[i])
+        {
+            for (int j = i * i; j <= k; j += i)
+            {
+                // mark the composite of the current prime to be true
+                notPrimes[j] = true;
+            }
+        }
+    }
+
+    for (int i = 2; i <= k; i++)
+    {
+        // if the number is prime
+        if (!notPrimes[i])
+        {
+            primes.push_back(i);
+        }
+    }
+
+    return primes;
+}
+
+void kthPrime()
+{
+    int q, n;
+    cin >> q;
+    vector<int> sieve = kthPrimeHelper();
+    while (q--)
+    {
+        cin >> n;
+        cout << "queries left: " << q << endl;
+        cout << "query: " << n << endl;
+        cout << "sieve size: " << sieve.size() << endl;
+        cout << sieve[n - 1] << endl;
+    }
+}
