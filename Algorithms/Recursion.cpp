2e85e5ded4687ce6b15a0480e186db3521a448e2
Michael Wilson <amcwils@amazon.com>
2020-10-31 11:53:28 -0700

    updated the recurisve portion of with a problem defining all subsets of a string

--- Algorithms/Recursion.cpp
+++ Algorithms/Recursion.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <string>
+#include <bitset>
 
 using namespace std;
 
 const int DEFAULT_SIZE = 5;
+const int CHARACTER_SIZE = 8;
 
 ///////////////////////////
 // FUNCTION DECLARATIONS //
 ///////////////////////////
+void findSubsets(string);
 void mergeSort(int *);
+
+//////////////////////////////////
+// HELPER FUNCTION DECLARATIONS //
+//////////////////////////////////
+void findSubsetsTests();
+void printSubset(string, int);
+void mergeSortTests();
 void mergeSortUtil(int *, int, int);
 void merge(int *, int, int, int);
 void printArray(int *);
 
 int main()
 {
-    int A[] = {5, 4, 3, 2, 1};
-    printf("Original array:\n");
-    printArray(A);
-    mergeSort(A);
-    printf("Sorted array:\n");
-    printArray(A);
+    //mergeSortTests();
+    findSubsetsTests();
 }
 
 //////////////////////////////
@@ -30,6 +37,46 @@ int main()
 void mergeSort(int *A)
 {
     mergeSortUtil(A, 0, DEFAULT_SIZE - 1);
+}
+
+void findSubsets(string str)
+{
+    int totalSetCount = 1 << str.length();
+    for (int i = 0; i < totalSetCount; i++)
+    {
+        printSubset(str, i);
+    }
+}
+
+/////////////////////////////////////
+// HELPER FUNCTION IMPLEMENTATIONS //
+/////////////////////////////////////
+void printSubset(string str, int bitNum)
+{
+    int len = str.length();
+    if (len < 1)
+        return;
+
+    printf("[|%c|", (bitNum >> 0) & 1 ? str[0] : ' ');
+    for (int i = 1; i < len; i++)
+    {
+        //printf("bit: %c", bits[i]);
+        if ((bitNum >> i) & 1)
+        {
+            printf("|%c|", str[i]);
+        }
+        else
+        {
+            printf("| |");
+        }
+    }
+    printf("]\n");
+}
+
+void findSubsetsTests()
+{
+    string str = "abc";
+    findSubsets(str);
 }
 
 void mergeSortUtil(int *A, int lo, int hi)
@@ -85,4 +132,14 @@ void printArray(int *A)
         printf(", %d", A[i]);
     }
     printf("]\n");
+}
+
+void mergeSortTests()
+{
+    int A[] = {5, 4, 3, 2, 1};
+    printf("Original array:\n");
+    printArray(A);
+    mergeSort(A);
+    printf("Sorted array:\n");
+    printArray(A);
 }
